8897bc83da852d5878db9091feb83829eb3a240e
Bahadir <bahadrix@gmail.com>
2020-01-24 17:51:51 +0300

    init examples

---
+++ examples/webpack.config.js
@@ -0,0 +1,35 @@
+const { resolve } = require('path');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const config = {
+  entry: {
+    Basic: resolve('./basic/src/index.ts')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: ['awesome-typescript-loader?module=es6'],
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.js$/,
+        loader: 'source-map-loader',
+        enforce: 'pre'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+  output: {
+    path: resolve('lib'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: '[name]',
+    umdNamedDefine: true
+  }
+};
+
+module.exports = config;
