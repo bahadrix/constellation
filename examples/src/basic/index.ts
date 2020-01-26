e58f8b7a8419343627228cb4b249bb9c8361bfea
Bahadir <bahadrix@gmail.com>
2020-01-26 17:36:12 +0300

    example dev. ready

---
+++ examples/src/basic/index.ts
@@ -0,0 +1,9 @@
+import Constellation from '../../../dist/index'
+
+
+let constellation = new Constellation({
+    sceneElement: document.getElementById('scene'),
+    width: 800,
+    height: 600
+})
+
